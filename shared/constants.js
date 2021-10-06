550421fabfc8082547fc4c78d6c64b5306cbfdb2
vanthieu.nguyen <vanthieu.nguyen@shopee.com>
2021-10-06 22:19:00 +0700

    change regex

--- shared/constants.js
+++ shared/constants.js
@@ -1,4 +1,4 @@
-export const PHONE_REGEX = /(\+84|0)+(3[2-9]|5[6|8|9]|9\d(?!5)|8[1-9]|7[0|6-9])+([0-9]{7})\b/
+export const PHONE_REGEX = /^(84|0)+(3[2-9]|5[6|8|9]|9\d(?!5)|8[1-9]|7[0|6-9])+([0-9]{7})\b/
 
 export const DATE_FORMAT = 'DD/MM/YYYY'
 
